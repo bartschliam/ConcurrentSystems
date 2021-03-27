42e17223325749708ed7ddf489d573b63e10dbd5
bartschliam <bartschl@tcd.ie>
2021-03-27 18:37:19 +0100

    Added for loop in sales.c to get average time

--- src/mytour.c
+++ src/mytour.c
@@ -7,24 +7,24 @@
 
 void my_tour(const point cities[], int tour[], int ncities)
 {
-	#pragma omp parallel
+	/*#pragma omp parallel
 	{
 		int NCPU,tid,NPR,NTHR;
-		/* get the total number of CPUs/cores available for OpenMP */
+		get the total number of CPUs/cores available for OpenMP
 		NCPU = omp_get_num_procs();
-		/* get the current thread ID in the parallel region */
+		get the current thread ID in the parallel region
 		tid = omp_get_thread_num();
-		/* get the total number of threads available in this parallel region */
+		get the total number of threads available in this parallel region
 		NPR = omp_get_num_threads();
-		/* get the total number of threads requested */
+		get the total number of threads requested
 		NTHR = omp_get_max_threads();
-		/* only execute this on the master thread! */
+		only execute this on the master thread!
 		if (tid == 0) {
 	  		printf("%i : NCPU = %i\n",tid,NCPU);
 	  		printf("%i : NTHR = %i\n",tid,NTHR);
 	  		printf("%i : NPR  = %i\n",tid,NPR);
 		}
-	}
+	}*/
 	int i, j;
 	char *visited = alloca(ncities);
 	int ThisPt, ClosePt = 0;
--- src/sales.c
+++ src/sales.c
@@ -141,11 +141,17 @@ int main(int argc, char *argv[])
   initialize_cities(cities, ncities, seed);
 
   /* find tour through the cities */
-  gettimeofday(&start_time, NULL);
-  call_student_tour(cities,tour,ncities);
-  gettimeofday(&stop_time, NULL);
-  compute_time = (stop_time.tv_sec - start_time.tv_sec) * 1000000L +
-    (stop_time.tv_usec - start_time.tv_usec);
+  int run_times = 1000;
+  int average = 0;
+  for(int i = 0; i < run_times; i++){
+    gettimeofday(&start_time, NULL);
+    call_student_tour(cities,tour,ncities);
+    gettimeofday(&stop_time, NULL);
+    compute_time = (stop_time.tv_sec - start_time.tv_sec) * 1000000L +
+      (stop_time.tv_usec - start_time.tv_usec);
+    average = average + compute_time;
+  }
+  compute_time = average / run_times;
   printf("Time to find tour: %lld microseconds\n", compute_time);
 
   /* check that the tour we found is correct */
