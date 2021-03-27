72f6657808613100f8651f7dcf30c681edee2017
bartschliam <bartschl@tcd.ie>
2021-03-27 17:51:22 +0100

    Added finding path tour inside mytour.c

--- src/mytour.c
+++ src/mytour.c
@@ -1,5 +1,8 @@
 #include "mytour.h"
 #include <omp.h>
+#include <alloca.h>
+#include <math.h>
+#include <float.h>
 
 
 void my_tour(const point cities[], int tour[], int ncities)
@@ -22,5 +25,28 @@ void my_tour(const point cities[], int tour[], int ncities)
 	  		printf("%i : NPR  = %i\n",tid,NPR);
 		}
 	}
-	simple_find_tour(cities, tour, ncities);
+	int i,j;
+	char *visited = alloca(ncities);
+	int ThisPt, ClosePt=0;
+	float CloseDist;
+	int endtour=0;
+	for (i=0; i<ncities; i++)
+    visited[i]=0;
+	ThisPt = ncities-1;
+	visited[ncities-1] = 1;
+	tour[endtour++] = ncities-1;
+	for (i=1; i<ncities; i++) {
+	    CloseDist = DBL_MAX;
+	    for (j=0; j<ncities-1; j++) {
+	      if (!visited[j]) {
+	        if (sqrt((cities[ThisPt].x-cities[j].x)*(cities[ThisPt].x-cities[j].x)+(cities[ThisPt].y-cities[j].y)*(cities[ThisPt].y-cities[j].y)) < CloseDist) {
+	          CloseDist = sqrt((cities[ThisPt].x-cities[j].x)*(cities[ThisPt].x-cities[j].x)+(cities[ThisPt].y-cities[j].y)*(cities[ThisPt].y-cities[j].y));
+	          ClosePt = j;
+	        }
+	      }
+	    }
+	    tour[endtour++] = ClosePt;
+	    visited[ClosePt] = 1;
+	    ThisPt = ClosePt;
+	  }
 }
