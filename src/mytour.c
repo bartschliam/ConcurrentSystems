4656bb179fba9feee0a55d8af9987b057af1163e
bartschliam <bartschl@tcd.ie>
2021-03-28 08:49:46 +0200

    Working version

--- src/mytour.c
+++ src/mytour.c
@@ -7,32 +7,13 @@
 
 void my_tour(const point cities[], int tour[], int ncities)
 {
-	/*#pragma omp parallel
-	{
-		int NCPU,tid,NPR,NTHR;
-		get the total number of CPUs/cores available for OpenMP
-		NCPU = omp_get_num_procs();
-		get the current thread ID in the parallel region
-		tid = omp_get_thread_num();
-		get the total number of threads available in this parallel region
-		NPR = omp_get_num_threads();
-		get the total number of threads requested
-		NTHR = omp_get_max_threads();
-		only execute this on the master thread!
-		if (tid == 0) {
-	  		printf("%i : NCPU = %i\n",tid,NCPU);
-	  		printf("%i : NTHR = %i\n",tid,NTHR);
-	  		printf("%i : NPR  = %i\n",tid,NPR);
-		}
-	}*/
 	int i, j;
 	char *visited = alloca(ncities);
 	int ThisPt, ClosePt = 0;
 	float CloseDist;
 	int endtour = 0;
-	#pragma omp parallel for
 	for (i = 0; i < ncities; i++){
-    	visited[i] = 0;
+		visited[i] = 0;
 	}
 	ThisPt = ncities - 1;
 	visited[ncities - 1] = 1;
