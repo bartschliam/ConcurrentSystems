49b9439297692c739881ab806607e59629f59d82
RuikisR <raivoruikis@gmail.com>
2021-04-01 13:51:01 +0100

    Added: Some omp jazz

--- src/mytour.c
+++ src/mytour.c
@@ -7,31 +7,44 @@
 
 void my_tour(const point cities[], int tour[], int ncities)
 {
-	int i,j;
+	int i;
   	char *visited = calloc(ncities, 1);
   	int ThisPt, ClosePt = 0;
-  	float CloseDist, dist;
-  	float x, y, thisX, thisY;
+  	float CloseDist;
+  	float thisX, thisY;
   	int endtour = 0;
   	ThisPt = ncities - 1;
   	visited[ncities - 1] = 1;
   	tour[endtour++] = ncities - 1;
 	  
 	for (i = 1; i < ncities; i++) {
-	    CloseDist = DBL_MAX;
+	    
 	    thisX = cities[ThisPt].x;
 	    thisY = cities[ThisPt].y;
-	    for (j = 0; j < ncities - 1; j++) {
-	      	if (!visited[j]) {
-	      		x = thisX - cities[j].x;
-	      		y = thisY - cities[j].y;
-	      		dist = sqrt(sqr(x) + sqr(y));
-	        	if (dist < CloseDist) {
-	          		CloseDist = dist;
-	          		ClosePt = j;
-	        	}
-	      	}
-	    }
+	    float CloseDist = DBL_MAX;
+	    #pragma omp parallel 
+	    {
+	   		float localCloseDist = CloseDist;
+	   		int lowestJ = 0;
+	   		#pragma omp for
+		    for (int j = 0; j < ncities - 1; j++) {
+		      	if (!visited[j]) {
+		      		float x = thisX - cities[j].x;
+		      		float y = thisY - cities[j].y;
+		      		float dist = sqrt(sqr(x) + sqr(y));
+		        	if (dist < localCloseDist) {
+		          		localCloseDist = dist;
+		          		lowestJ = j;
+		        	}
+		      	}
+		    }
+		    #pragma omp critical
+		    if (localCloseDist < CloseDist) {
+		    	CloseDist = localCloseDist;
+		    	ClosePt = lowestJ;
+
+		    }
+		}
 	    tour[endtour++] = ClosePt;
 	    visited[ClosePt] = 1;
 	    ThisPt = ClosePt;
