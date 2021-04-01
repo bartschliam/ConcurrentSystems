df4ed40735ba15fed83e9db89912d3e4c9c930cb
RuikisR <raivoruikis@gmail.com>
2021-04-01 13:09:17 +0100

    Ready for threading

--- src/mytour.c
+++ src/mytour.c
@@ -1,32 +1,31 @@
 #include "mytour.h"
 #include <omp.h>
-#include <alloca.h>
 #include <math.h>
 #include <float.h>
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #define sqr(a) a * a
 
 void my_tour(const point cities[], int tour[], int ncities)
 {
 	int i,j;
-  	char *visited = alloca(ncities);
+  	char *visited = calloc(ncities, 1);
   	int ThisPt, ClosePt = 0;
   	float CloseDist, dist;
+  	float x, y, thisX, thisY;
   	int endtour = 0;
-  
-  	for (i = 0; i < ncities; i++) {
-    	visited[i] = 0;
-  	}
   	ThisPt = ncities - 1;
   	visited[ncities - 1] = 1;
   	tour[endtour++] = ncities - 1;
 	  
 	for (i = 1; i < ncities; i++) {
 	    CloseDist = DBL_MAX;
+	    thisX = cities[ThisPt].x;
+	    thisY = cities[ThisPt].y;
 	    for (j = 0; j < ncities - 1; j++) {
 	      	if (!visited[j]) {
-	      		dist = sqrt(sqr((cities[ThisPt].x-cities[j].x))+sqr((cities[ThisPt].y-cities[j].y)));
+	      		x = thisX - cities[j].x;
+	      		y = thisY - cities[j].y;
+	      		dist = sqrt(sqr(x) + sqr(y));
 	        	if (dist < CloseDist) {
 	          		CloseDist = dist;
 	          		ClosePt = j;
