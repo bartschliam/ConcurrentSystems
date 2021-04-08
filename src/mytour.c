39f5c19123541590149c75ae1d96296c42bae3b7
RedmondC <colinredmond@protonmail.com>
2021-04-08 10:37:12 +0100

    removed unneccesary comment in mytour

--- src/mytour.c
+++ src/mytour.c
@@ -1,4 +1,3 @@
-
 #include "mytour.h"
 #include <omp.h>
 #include <math.h>
@@ -84,56 +83,3 @@ void my_tour(const point cities[], int tour[], int ncities)
 	}
 	free(visited);
 }
-
-/*
-
-	Single Thread Optimised
-	Just Remove the pragmas - some parts can be further optimised
-
-void my_tour(const point cities[], int tour[], int ncities)
-{
-	int i;
-  	char *visited = calloc(ncities, 1);
-  	int ThisPt, ClosePt = 0;
-  	float CloseDist;
-  	float thisX, thisY;
-  	int endtour = 0;
-  	ThisPt = ncities - 1;
-  	visited[ncities - 1] = 1;
-  	tour[endtour++] = ncities - 1;
-	  
-	for (i = 1; i < ncities; i++) {
-	    
-	    thisX = cities[ThisPt].x;
-	    thisY = cities[ThisPt].y;
-	    float CloseDist = DBL_MAX;
-	    #pragma omp parallel 
-	    {
-	   		float localCloseDist = CloseDist;
-	   		int lowestJ = 0;
-	   		#pragma omp for
-		    for (int j = 0; j < ncities - 1; j++) {
-		      	if (!visited[j]) {
-		      		float x = thisX - cities[j].x;
-		      		float y = thisY - cities[j].y;
-		      		float dist = sqrt(sqr(x) + sqr(y));
-		        	if (dist < localCloseDist) {
-		          		localCloseDist = dist;
-		          		lowestJ = j;
-		        	}
-		      	}
-		    }
-		    #pragma omp critical
-		    if (localCloseDist < CloseDist) {
-		    	CloseDist = localCloseDist;
-		    	ClosePt = lowestJ;
-
-		    }
-		}
-	    tour[endtour++] = ClosePt;
-	    visited[ClosePt] = 1;
-	    ThisPt = ClosePt;
-	}
-}
-
-*/
